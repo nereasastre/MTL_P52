1ecff765cc376f05459893dc039b1c64d1928882
nereasastre <nerea.sastre01@estudiant.upf.edu>
2022-04-28 13:46:41 +0200

    removed local files

--- offline_pitch_extractor/__init__.py
+++
@@ -1,5 +0,0 @@
-import crepe
-from scipy.io import wavfile
-
-sr, audio = wavfile.read("C:/Users/nersa/OneDrive/Documents/MTL/MTL_P52/sounds/piano.wav")
-time, frequency, confidence, activation = crepe.predict(audio, sr, viterbi=True)
