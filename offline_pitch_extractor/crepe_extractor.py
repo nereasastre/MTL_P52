df68ccd2e0f511d5db3a9db166a2d970eee19430
APPIAHTECH <APPIAHTECH@users.noreply.github.com>
2022-05-08 16:30:14 +0000

    :art: Format Python code with psf/black

--- offline_pitch_extractor/crepe_extractor.py
+++ offline_pitch_extractor/crepe_extractor.py
@@ -2,7 +2,7 @@ import crepe
 from scipy.io import wavfile
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 
 def crepe_pitch(audio, sr):
@@ -23,16 +23,16 @@ def crepe_pitch(audio, sr):
     for idx in range(len(frequency) - 1):
         if confidence[idx] > 0.8:
             if not idxs:
-                idxs.append(idx) # append first decent guess
+                idxs.append(idx)  # append first decent guess
         if abs(frequency[idx] - frequency[idx + 1]) > 3:
-            if idx !=0:
+            if idx != 0:
                 idxs.append(idx)
 
     print("Detected frequency: ", frequency[idxs][0].round())
 
-    #print("time: ", time[idxs], "frequency: ", frequency[idxs], "confidence: ", confidence[idxs])
-    #print("time: ", time, "frequency: ", frequency, "confidence: ", confidence)
-    #print(len(audio), "\n", len(time), "\n",  len(frequency), "\n",len(confidence),  "\n", len(activation))
+    # print("time: ", time[idxs], "frequency: ", frequency[idxs], "confidence: ", confidence[idxs])
+    # print("time: ", time, "frequency: ", frequency, "confidence: ", confidence)
+    # print(len(audio), "\n", len(time), "\n",  len(frequency), "\n",len(confidence),  "\n", len(activation))
     return time, frequency, confidence, activation
 
 
