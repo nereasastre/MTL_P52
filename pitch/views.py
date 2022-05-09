e4ae2fb9f8484b3c1c775db67025256f65cc07ce
Stephen Appiah <stephenappiahtech@gmail.com>
2022-05-09 20:42:55 +0200

    test

--- pitch/views.py
+++ pitch/views.py
@@ -5,7 +5,7 @@ import logging
 def index_view(request):
     try:
         template_name = "pitch_extraction.html"
-        print("hola mundo")
+        print("hola mund ""asjkfsakfo")
         context = {}
         return render(request, template_name, context)
     except Exception as e:
