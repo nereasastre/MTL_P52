3d7e7fa204fc2180c5971205a37fe870b9d96cf4
Mario229997 <mario.torres01@estudiant.upf.edu>
2022-05-11 11:25:55 +0200

    Add pitch

---
+++ offline_pitch_extractor/fft_extractor.py
@@ -0,0 +1,43 @@
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
+from scipy.signal import find_peaks
+
+
+def fft_pitch(path):
+    # Load data and sampling frequency from the data file
+    sampling_frequency, data = wavfile.read(path)
+
+    # Get some useful statistics
+    T = 1/sampling_frequency  # Sampling period
+    N = data.size  # Signal length in samples
+    t = N / sampling_frequency  # Signal length in seconds
+
+    Y_k = np.fft.fft(data)[0:int(N/2)]/N  # FFT
+    Y_k[1:] = 2*Y_k[1:]  # Single-sided spectrum
+    Pxx = np.abs(Y_k)  # Power spectrum
+
+    f = sampling_frequency * np.arange((N/2)) / N  # frequencies
+
+    auto = sm.tsa.acf(data, nlags=2000)
+    peaks = find_peaks(auto)[0]  # Find peaks of the autocorrelation
+    lag = peaks[0]  # Choose the first peak as our pitch component lag
+    pitch = sampling_frequency / lag  # Transform lag into frequency
+    print('Pitch detected: ', pitch)
+
+    # plotting
+    '''
+    fig, ax = plt.subplots()
+    plt.plot(f[0:2000], Pxx[0:2000], linewidth=2)
+    plt.ylabel('Amplitude')
+    plt.xlabel('Frequency [Hz]')
+    plt.show()
+    '''
+
+    return pitch
+
+
+path = "../sounds/violin-B3.wav"
+pitch = fft_pitch(path)
+
