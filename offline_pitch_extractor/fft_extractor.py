ce0ec16f736c8fb2723e50d61fc8030d53683f87
Mario229997 <Mario229997@users.noreply.github.com>
2022-05-11 10:13:23 +0000

    :art: Format Python code with psf/black

--- offline_pitch_extractor/fft_extractor.py
+++ offline_pitch_extractor/fft_extractor.py
@@ -10,36 +10,34 @@ def fft_pitch(path):
     sampling_frequency, data = wavfile.read(path)
 
     # Get some useful statistics
-    T = 1/sampling_frequency  # Sampling period
+    T = 1 / sampling_frequency  # Sampling period
     N = data.size  # Signal length in samples
     t = N / sampling_frequency  # Signal length in seconds
 
-    Y_k = np.fft.fft(data)[0:int(N/2)]/N  # FFT
-    Y_k[1:] = 2*Y_k[1:]  # Single-sided spectrum
+    Y_k = np.fft.fft(data)[0 : int(N / 2)] / N  # FFT
+    Y_k[1:] = 2 * Y_k[1:]  # Single-sided spectrum
     Pxx = np.abs(Y_k)  # Power spectrum
 
-    f = sampling_frequency * np.arange((N/2)) / N  # frequencies
+    f = sampling_frequency * np.arange((N / 2)) / N  # frequencies
 
     auto = sm.tsa.acf(data, nlags=2000)
     peaks = find_peaks(auto)[0]  # Find peaks of the autocorrelation
     lag = peaks[0]  # Choose the first peak as our pitch component lag
     pitch = sampling_frequency / lag  # Transform lag into frequency
-    print('Pitch detected: ', pitch)
+    print("Pitch detected: ", pitch)
 
     # plotting
 
     fig, ax = plt.subplots()
     plt.plot(f[0:2000], Pxx[0:2000], linewidth=2)
-    plt.ylabel('Amplitude')
-    plt.xlabel('Frequency [Hz]')
+    plt.ylabel("Amplitude")
+    plt.xlabel("Frequency [Hz]")
     plt.show()
-
 
     return pitch
 
 
 path = "../sounds/violin-B3.wav"
-#path = "../sounds/sine-101.wav"
+# path = "../sounds/sine-101.wav"
 
 pitch = fft_pitch(path)
-
