8b3b7d7b27c4088c4a384409174e72c1e8b3a9cd
Mario229997 <mario.torres01@estudiant.upf.edu>
2022-05-18 12:18:41 +0200

    find_closest_note: Function to find the closest note of the detected pitch.
    record_sound: Function to record with the computer mic a sound that will be saved in an audio file.

---
+++ real_time_pitch_extractor/record_sound.py
@@ -0,0 +1,20 @@
+import sounddevice as sd
+import scipy.io.wavfile
+import time
+
+SAMPLE_FREQ = 44100  # Sampling frequency of the recording
+SAMPLE_DUR = 2  # Duration of the recoding
+
+print("Grab your guitar!")
+time.sleep(1)   # Gives you a second to grab your guitar ;)
+
+myRecording = sd.rec(SAMPLE_DUR * SAMPLE_FREQ, samplerate=SAMPLE_FREQ, channels=1, dtype='float64')
+print("Recording audio")
+sd.wait()
+
+sd.play(myRecording, SAMPLE_FREQ)
+print("Playing audio")
+sd.wait()
+
+scipy.io.wavfile.write('example1.wav', SAMPLE_FREQ, myRecording)
+
