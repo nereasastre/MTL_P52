24df78ecb418cb993937290f43269019cb78f714
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-05-18 12:22:08 +0200

    improvement pitch_extractor

--- offline_pitch_extractor/pitch_extractor.py
+++ offline_pitch_extractor/pitch_extractor.py
@@ -30,10 +30,14 @@ def extract_pitch():
               "    5. YIN \n")
     )  # Extractor type todo account for wrong inputs
 
-    # record and load audio
+    """# record and load audio
     recording_path = "../sound_recorder/output.wav"
     record_and_export(recording_path)
-    sr, audio = wavfile.read(recording_path)
+    sr, audio = wavfile.read(recording_path)"""
+
+    # record and load audio
+    path = "../sounds/sine-101.wav"
+    sr, audio = wavfile.read(path)
 
     # Extract pitch calling the corresponding extractor
 
@@ -41,11 +45,16 @@ def extract_pitch():
 
     freq = extractors[extractor_idx-1](audio, sr)
 
+    # measures of efficiency
     end_time = time.time()
     execution_time = end_time - start_time
 
+    audio_dur = len(audio)/sr
+    rtf = execution_time/audio_dur
+
     # Print some info
     print("Execution time %s seconds" % execution_time)
+    print("Real-Time factor = %f" % rtf)
     print("Detected frequency: ", freq)
 
 
