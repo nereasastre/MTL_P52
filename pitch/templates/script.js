3b9bce89ce4b90f6e064219bc28b80de150c3fde
unknown <78813878+umarmh9@users.noreply.github.com>
2022-05-20 17:43:17 +0200

    design1-dropdown

---
+++ pitch/templates/script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+function getLocalStream() {
+    navigator.mediaDevices.getUserMedia({video: false, audio: true}).then( stream => {
+        window.localStream = stream; // A
+        window.localAudio.srcObject = stream; // B
+        window.localAudio.autoplay = true; // C
+    }).catch( err => {
+        console.log("u got an error:" + err)
+    });
+}
